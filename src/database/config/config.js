1d0e6251188ef62d266da907ffeed39fe0f28341
ichristine180 <ichristine180@gmail.com>
2023-05-19 00:09:19 +0200

    config

---
+++ src/database/config/config.js
@@ -0,0 +1,21 @@
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
+  },
+  production : {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+}
--- src/database/config/db.js
+++ src/database/config/db.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import config from './config.json' assert { type: "json" };;
+import config from './config.js';
 const env = process.env.NODE_ENV || 'development';
 const sequelizeConfig = config[env];
 const sequelize = new Sequelize(sequelizeConfig.database, sequelizeConfig.username, sequelizeConfig.password, {
