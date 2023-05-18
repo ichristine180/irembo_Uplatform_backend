d91f743c4f18bd618820f8b98db8f988384bded6
ichristine180 <ichristine180@gmail.com>
2023-05-19 00:37:10 +0200

    redis

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import redis from "redis";
 dotenv.config();
 //creating redis server for caching data
 export const redisAsyncClient = redis.createClient(
-  process.env.PORT || 6379,
+  // process.env.PORT || 6379,
   process.env.REDIS_URL
 );
 await redisAsyncClient.connect();
